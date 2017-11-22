7a8e87e9c572e284afc22c405af16a6236086f91
Cat Yanish <catyanish@gmail.com>
2017-11-21 20:53:28 -0600

    built and injected an angular 2 service, finally got wires uncrossed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PeopleListComponent } from './people-list/people-list.component';
-
+import { PeopleService } from './people.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,7 @@ import { PeopleListComponent } from './people-list/people-list.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [ PeopleService] // You can put your services here!
 })
 export class AppModule { }
--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -1,25 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from '../person';
+import { PeopleService } from "../people.service";
 
 @Component({
   selector: 'app-people-list',
   template: `
-    <ul>
-      <li *ngFor="let person of people">
-      {{person.name}}
-      </li>
-    </ul>
+  <!-- this is the new syntax for ng-repeat -->
+  <ul>
+    <li *ngFor="let person of people">
+     {{person.name}}
+    </li>
+  </ul>
   `,
   styleUrls: ['./people-list.component.scss']
 })
 export class PeopleListComponent implements OnInit {
-  people: Person[] = [
-   {name: 'Luke Skywalker', height: 177, weight: 70},
-   {name: 'Darth Vader', height: 200, weight: 100},
-   {name: 'Han Solo', height: 185, weight: 85},
- ];
-  constructor() { }
+  people: Person[];
 
-  ngOnInit() {
+  constructor(private peopleService: PeopleService) {
+    this.people = peopleService.getAll();
   }
 
+  ngOnInit() {}
 }
